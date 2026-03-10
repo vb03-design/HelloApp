5e0108278a808484f100d55503619650aef32d9c
Vaishnavi <vaishnavibharat.170807@gmail.com>
2026-03-10 15:37:01 +0530

    UC3: Render OOPS banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,25 @@
 /**
- * OOPS Banner Application
- * Displays the word "OOPS" as a banner using stars
+ * OOPSBannerApp UC3 – Banner Application using String.join()
+ *
+ * Refactors UC2 by replacing string concatenation (+)
+ * with String.join() while producing the same banner output.
  *
  * @author Vaishnavi
- * @version 2.0
+ * @version 3.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" ***   ***   *****   ***** ");
-        System.out.println("*   * *   *  *   *  *      ");
-        System.out.println("*   * *   *  *   *  *      ");
-        System.out.println("*   * *   *  *****   ***** ");
-        System.out.println("*   * *   *  *            *");
-        System.out.println("*   * *   *  *            *");
-        System.out.println(" ***   ***   *       *****");
-
+        System.out.println(String.join(" ", "   ***  ", "    ***  ", " ****** ", "    ***** "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **     "));
+        System.out.println(String.join(" ", "**     **", "**     **", "**    ** ", "**     "));
+        System.out.println(String.join(" ", "**     **", "**     **", "****** ", "    ***** "));
+        System.out.println(String.join(" ", "**     **", "**     **", "**     ", "         **"));
+        System.out.println(String.join(" ", "**     **", "**     **", "**     ", "          **"));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "**     ", "         ** "));
+        System.out.println(String.join(" ", "   ***  ", "    ***  ", " **     ", "    ***** "));
+        System.out.println(String.join(" ", "       ", "       ", "      ", "       "));
     }
 }
